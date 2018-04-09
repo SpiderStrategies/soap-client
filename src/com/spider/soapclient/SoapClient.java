0fbf613f477986f39206f71c1b8605170048ba95
jason.mckim <jason.mckim@spiderstrategies.com>
2018-04-09 14:34:53 -0400

    count properly

--- src/com/spider/soapclient/SoapClient.java
+++ src/com/spider/soapclient/SoapClient.java
@@ -3,8 +3,9 @@ package com.spider.soapclient;
 public class SoapClient {
 
 	public static void main(String[] args) throws Exception {
-		if(args.length < 6) {
+		if(args.length < 7) {
 			System.err.println("Usage: java SoapClient url soapBodyFileLoc soapHeaderFileLoc soapVersion(1.1 or 1.2) soapAction username password");
+			System.exit(0);
 		}
 		String url = args[0];
 		String soapBody = Utils.readTextFromFile(args[1]);
