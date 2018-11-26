3c0d6ac0e24358da11ead09afe65728d202e8819
jason.mckim <jason.mckim@spiderstrategies.com>
2018-11-26 12:33:53 -0500

    move to gradle build

--- src/main/java/com/spider/soapclient/SoapClient.java
+++ src/main/java/com/spider/soapclient/SoapClient.java
@@ -3,19 +3,26 @@ package com.spider.soapclient;
 public class SoapClient {
 
 	public static void main(String[] args) throws Exception {
-		if(args.length < 7) {
-			System.err.println("Usage: java SoapClient url soapBodyFileLoc soapHeaderFileLoc soapVersion(1.1 or 1.2) soapAction username password");
+		if(args.length < 4) {
+			System.err.println("Usage: java -cp soap-client.jar url soapBodyFileLoc soapHeaderFileLoc soapVersion(1.1 or 1.2) soapAction username password");
 			System.exit(0);
 		}
 		String url = args[0];
 		String soapBody = Utils.readTextFromFile(args[1]);
 		String soapHeader = Utils.readTextFromFile(args[2]);
 		String soapVersion = "SOAP " + args[3] + " Protocol";
-		String soapAction = args[4];
-		String username = args[5];
-		String password = args[6];
+		String soapAction = getOptionalParam(args, 4);
+		String username = getOptionalParam(args, 5);
+		String password = getOptionalParam(args, 6);
 		String output = SOAPSender.send(url, soapBody, soapHeader, soapAction, soapVersion, username, password);
 		System.out.println(output);
+	}
+
+	private static String getOptionalParam(String[] args, int index) {
+		if(args.length > index) {
+			return args[index];
+		}
+		return null;
 	}
 	
 }
